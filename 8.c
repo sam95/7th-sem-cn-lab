9b1ada6a06f1a0a809751a8b92e9a393c618ea53
Sameera S <sameera1595@gmail.com>
2015-11-14 10:27:48 +0530

    code fixed

--- 8.c
+++ 8.c
@@ -9,42 +9,37 @@ struct node
 
 void main()
 {
-	int a[15][15],n=0,i,j,k,count=0;
+	int a[15][15],n=0,i,j,k;
 	struct node s[10];
 	printf("enter number of nodes\n");
 	scanf("%d",&n);
 	printf("enter matrix\n");
-	for(i=0;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
-		for(j=0;j<n;j++)
+		for(j=1;j<=n;j++)
 		{
 			scanf("%d",&a[i][j]);
 			s[i].dist[j]=a[i][j];
 			s[i].from[j]=j;
 		}
 	}
-	do
+	for(k=1;k<=n;k++)
 	{
-		count=0;
-		for(k=0;k<n;k++)
+		for(i=1;i<=n;i++)
 		{
-			for(i=0;i<n;i++)
+			for(j=1;j<=n;j++)
 			{
-				for(j=0;j<n;j++)
+				if(s[i].dist[j] > a[i][k] + s[k].dist[j])
 				{
-					if(s[i].dist[j] > a[i][k] + s[k].dist[j])
-					{
-						s[i].dist[j] = a[i][k] + s[k].dist[j];
-						s[i].from[j]=k;
-						count++;
-					}
+					s[i].dist[j] = a[i][k] + s[k].dist[j];
+					s[i].from[j]=k;
 				}
 			}
 		}
-	}while(count!=n);
-	for(i=0;i<n;i++)
+	}
+	for(i=1;i<=n;i++)
 	{
-		for(j=0;j<n;j++)
+		for(j=1;j<=n;j++)
 		{
 			printf("Src : %d -> Dest : %d Next node : %d Distance : %d\n",i,j,s[i].from[j],s[i].dist[j]);
 		}
